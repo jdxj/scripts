87c5f8b0c1594824532b837bedec7a50cee269f7
jdxj <jdxj@chives.life>
2025-12-09 23:44:46 +0800

    refactor

--- install.vps.sh
+++ install.vps.sh
@@ -8,7 +8,7 @@ read -p "主机名: " -r name
 hostnamectl hostname "$name"
 
 # 安装必要软件
-apt install -y zsh curl git vim wget
+apt install -y zsh curl git vim wget make
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
