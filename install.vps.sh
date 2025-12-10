3e1490c1e35d42d18771c8a9703ba8b68c03f648
jdxj <jdxj@chives.life>
2025-12-10 18:51:52 +0800

    refactor

--- install.vps.sh
+++ install.vps.sh
@@ -20,16 +20,16 @@ sed -i '11s/robbyrussell/jonathan/' ~/.zshrc
 # tcp
 wget https://raw.githubusercontent.com/yeahwu/v2ray-wss/main/tcp-window.sh && bash tcp-window.sh
 
-
-# ssh 配置-----------------------------------------------------
-
+# ssh
+rm -rf /etc/ssh/sshd_config.d/*.conf
+tee /etc/ssh/sshd_config << EOF
 # 使用现代协议
 Protocol 2
 
 # 禁止 root 密码登录
 PermitRootLogin without-password
 
-# 禁止密码登录（只允许 SSH key）
+# 禁止密码登录（只允许 SSH key)
 PasswordAuthentication no
 ChallengeResponseAuthentication no
 KbdInteractiveAuthentication no
@@ -51,10 +51,7 @@ IgnoreRhosts yes
 HostbasedAuthentication no
 IgnoreUserKnownHosts yes
 
-# 若只允许某用户登录，可启用
-# AllowUsers youruser
-
-# 使用现代 HostKey（只用 ED25519 更快）
+# 使用现代 HostKey(只用 ED25519 更快）
 HostKey /etc/ssh/ssh_host_ed25519_key
 
 # 强制使用更强的加密算法（兼容现代系统）
@@ -70,6 +67,4 @@ MaxStartups 2:30:10
 
 # 记录详细日志
 LogLevel VERBOSE
-
-
-# ssh 配置-----------------------------------------------------
+EOF
