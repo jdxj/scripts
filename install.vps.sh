66133289edf42ae225a54c4dadcfebd715a95d9b
jdxj <jdxj@chives.life>
2026-01-10 13:59:06 +0800

    refactor: scripts

--- install.vps.sh
+++ install.vps.sh
@@ -19,7 +19,7 @@ hostnamectl hostname "$name"
 ssh-keygen
 
 # 安装必要软件
-apt install -y zsh curl git vim wget make net-tools dnsutils telnet nload chrony jq
+apt install -y zsh curl git vim wget make net-tools dnsutils telnet nload chrony jq tcpdump netcat-openbsd
 
 # zsh
 if [ ! -e ~/.zshrc ]; then
